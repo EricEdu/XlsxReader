c8c1421901bd8ed7542fb15ed891183d5e1a9c1f
Erick Eduardo <erickeduardotiberio@gmail.com>
2024-05-21 08:26:39 -0300

    first2

--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,39 @@
 import express from 'express';
 import multer from 'multer';
-import path from 'path';
-import xlsx from 'xlsx';
-import { User } from './models';
+import * as xlsx from 'xlsx';
+import { sequelize, User } from './models/index';
 
 const app = express();
 const port = 3000;
 
-// Definindo o armazenamento em memória
+app.use(express.json());
+
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
-
-app.use(express.json());
 
 app.post('/upload', upload.single('file'), async (req, res) => {
   if (!req.file) {
     return res.status(400).send('No file uploaded.');
   }
 
-  // Lendo os dados do arquivo diretamente da memória
-  const workbook = xlsx.read(req.file.buffer, { type: 'buffer' });
-  const sheetName = workbook.SheetNames[0];
-  const sheet = workbook.Sheets[sheetName];
-  const data = xlsx.utils.sheet_to_json<{ Name: string; Email: string }>(sheet);
+  try {
+    const workbook = xlsx.read(req.file.buffer, { type: 'buffer' });
+    const sheetName = workbook.SheetNames[0];
+    const sheet = workbook.Sheets[sheetName];
+    const data = xlsx.utils.sheet_to_json<{ Name: string; Email: string }>(sheet);
+    for (const row of data) {
+      await User.create({
+        name: row.Name,
+        email: row.Email,
+      });
+    }
 
-  // Exemplo de inserção no banco de dados
-  for (const row of data) {
-    await User.create({
-      name: row.Name,
-      email: row.Email,
-    });
+    // Responder com status 200 OK
+    res.sendStatus(200);
+  } catch (error) {
+    console.error('Error processing file:', error);
+    res.status(500).send('Error processing file');
   }
-
-  res.sendStatus(200);
 });
 
 app.listen(port, () => {
